78e2229f0607f30abe7398f5ebd5c8b219f11d70
贺宇成 <heyucheng001@ke.com>
2020-03-08 16:42:48 +0800

    Add Redux demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,127 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'beike/page/home_page.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+void main() => runApp(MyReduxApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class ReduxState {
+  String name;
+  String password;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+  ReduxState(this.name, this.password);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+ReduxState appReducer(ReduxState state, action) {
+  return ReduxState(
+      NameReducer(state.name, action), PasswordReducer(state.password, action));
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+final NameReducer = combineReducers<String>([
+  TypedReducer<String, UpdateNameAction>((name, action) {
+    return action.name;
+  }),
+]);
+
+final PasswordReducer = combineReducers<String>([
+  TypedReducer<String, UpdatePasswordAction>((password, action) {
+    return action.password;
+  }),
+]);
+
+class UpdateNameAction {
+  final name;
+
+  UpdateNameAction(this.name);
+}
+
+class UpdatePasswordAction {
+  final password;
+
+  UpdatePasswordAction(this.password);
+}
+
+class MyReduxApp extends StatelessWidget {
+  final store = new Store<ReduxState>(appReducer,
+      initialState: new ReduxState("未知", "123"));
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return StoreProvider(
+      store: store,
+      child: new MaterialApp(
+          home: Container(
+        color: Colors.white,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            StoreConnector<ReduxState, String>(
+              converter: (store) => store.state.name,
+              builder: (context, name) {
+                return new Text(
+                  name,
+                  style: Theme.of(context).textTheme.display1,
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            StoreConnector<ReduxState, String>(
+              converter: (store) => store.state.password,
+              builder: (context, password) {
+                return Text(password);
+              },
             ),
+            StoreConnector<ReduxState, VoidCallback>(
+              converter: (store) {
+                return () => store.dispatch(UpdateNameAction("修改名字成功"));
+              },
+              builder: (context, callback) {
+                return FlatButton(
+                  child: Text("点击修改姓名"),
+                  onPressed: () {
+                    callback();
+                  },
+                );
+              },
+            ),
+            StoreConnector<ReduxState, VoidCallback>(
+              converter: (store) {
+                return () => store.dispatch(UpdatePasswordAction("修改密码成功"));
+              },
+              builder: (context, callback) {
+                return FlatButton(
+                  child: Text("点击修改密码"),
+                  onPressed: () {
+                    callback();
+                  },
+                );
+              },
+            ),
+            StoreConnector<ReduxState, Void>(
+              builder: (context, value) {
+                return FlatButton(
+                  child: Text("of方式修改密码"),
+                  onPressed: () {
+                    StoreProvider.of<ReduxState>(context)
+                        .dispatch(UpdatePasswordAction("通过of修改密码成功"));
+                  },
+                );
+              }, converter: (Store store) {return null;},
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )),
     );
   }
 }
